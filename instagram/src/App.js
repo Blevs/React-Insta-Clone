8c4c9157ffb275f173a687800c4c812b96830b32
Henry Blevins <heblevi@gmail.com>
2019-03-25 16:47:36 -0400

    Style posts container

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import PostContainer from './components/PostContainer';
 import dummyData from './dummy-data.js';
+import styled from 'styled-components';
+
+const Posts = styled.div`
+display: flex;
+width: 100%;
+flex-flow: column;
+align-items: center;
+padding: 0 5%;
+box-sizing: border-box;
+background: #fafafa;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
             <SearchBar />
-        {dummyData.map(post => (
-            <PostContainer {...post} key={post.id} />
-        ))}
+        <Posts>
+          {dummyData.map(post => (
+              <PostContainer {...post} key={post.id} />
+          ))}
+        </Posts>
       </div>
     );
   }
