777b079e71767cf1f37576a06bad71e4be51b3c3
Henry Blevins <heblevi@gmail.com>
2019-03-25 15:02:00 -0400

    Rough out search bar

---
+++ instagram/src/components/SearchBar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SearchBar = (props) => {
+    return (
+        <header>
+          <div>
+            <img alt="logo"/>
+            <img alt="instagram"/>
+          </div>
+          <input type="text"
+                 placeholder="Search" />
+          <div>
+            <a href="/">D</a>
+            <a href="/">L</a>
+            <a href="/">P</a>
+          </div>
+        </header>
+    );
+};
+
+export default SearchBar;
