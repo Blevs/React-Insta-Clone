7f8ecd742cca89499906e3e700ee35e75008845c
Henry Blevins <heblevi@gmail.com>
2019-03-28 16:12:37 -0400

    Fix singup page also rending userpage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,16 +4,18 @@ import { withAuthenticate } from './authentication';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 import UserPage from './components/UserPage';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     const AuthedPostsPage = withAuthenticate(PostsPage)(LoginPage);
     return (
       <Router>
-        <Route exact path="/" component={AuthedPostsPage}/>
-        <Route path="/signup" component={SignupPage}/>
-        <Route path="/:username" component={UserPage} />
+        <Switch>
+          <Route exact path="/" component={AuthedPostsPage}/>
+          <Route exact path="/signup" component={SignupPage}/>
+          <Route path="/:username" component={UserPage} />
+        </Switch>
       </Router>
     );
   }
